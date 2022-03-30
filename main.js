6f973ad9b0acd1350cb29018bcd1d67110dc49f9
AlbertoBiga <alberto.biga10@gmail.com>
2022-03-30 15:25:21 +0200

    consegna

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data:{
+            text:'Ciao! Sto imparando ad usare Vue.js!',
+            img:'./img/RubberDuck.jpg',
+            imgClass:'img'
+        }
+    }
+);
